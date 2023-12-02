1e10d18e284af65d15ece15e8d445c08c36deccf
DillTheBeast <dillonmaltese@gmail.com>
2023-12-01 20:38:56 -0500

    Update test.py

--- test.py
+++ test.py
@@ -7,9 +7,21 @@ class CodeEditor:
         self.root = root
         self.root.title("Simple Code Editor")
 
-        # Create a scrolled text widget
-        self.text_widget = scrolledtext.ScrolledText(root, wrap=tk.WORD, width=500, height=500)
-        self.text_widget.pack(expand=True, fill='both')
+        # Create a Text widget with a vertical scrollbar
+        self.text_widget = tk.Text(root, wrap=tk.WORD, width=300, height=300)
+        self.text_widget.grid(row=0, column=0, sticky="nsew")
+
+        # Vertical scrollbar
+        scrollbar = tk.Scrollbar(root, command=self.text_widget.yview)
+        scrollbar.grid(row=0, column=1, sticky="ns")
+        self.text_widget.config(yscrollcommand=scrollbar.set)
+
+        # Configure row and column weights for expansion
+        root.grid_rowconfigure(0, weight=1)
+        root.grid_columnconfigure(0, weight=1)
+
+        # Bind click event to text widget
+        self.text_widget.bind("<Button-1>", self.text_widget_click)
 
         # Menu bar
         self.menu_bar = tk.Menu(root)
@@ -24,10 +36,18 @@ class CodeEditor:
         self.file_menu.add_command(label="Save As", command=self.save_as_file)
         self.file_menu.add_separator()
         self.file_menu.add_command(label="Exit", command=root.destroy)
-        
-        # Bind Command+S (or Control+S) to save_file function
+
+        # Adding Commands to work
         root.bind('<Command-s>', self.save_file)
         root.bind('<Control-s>', self.save_file)
+        root.bind('<Command-n>', self.new_file)
+        root.bind('<Control-n>', self.new_file)
+        root.bind('<Command-o>', self.open_file)
+        root.bind('<Control-o>', self.open_file)
+        root.bind('<Command-e>', self.custom_destroy)  # Rename destroy method
+
+    def text_widget_click(self, event):
+        self.text_widget.focus_set()
 
     def new_file(self):
         self.text_widget.delete(1.0, tk.END)
@@ -50,15 +70,24 @@ class CodeEditor:
             # If no file_path is associated, prompt for a file name to save as
             self.save_as_file()
 
-
     def save_as_file(self):
         file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
         if file_path:
+            # Check if the user entered a file extension
+            if not file_path.endswith((".txt", ".md", ".py")):
+                # If no file extension is provided, ask the user for the file type
+                file_type = filedialog.askstring("File Type", "Enter file type (e.g., txt, md, py):")
+                if file_type is not None:  # Check if file_type is not None
+                    # Add the file extension to the file_path
+                    file_path += f".{file_type.strip().lower()}"
+
             with open(file_path, 'w') as file:
                 content = self.text_widget.get(1.0, tk.END)
                 file.write(content)
             self.file_path = file_path
 
+    def custom_destroy(self):
+        self.root.destroy()
 
 if __name__ == "__main__":
     root = tk.Tk()
